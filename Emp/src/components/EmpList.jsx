bd2b45a4dc6141cad27d1809c45332515e2b637a
arjnramesh281 <arjunramesh281@gmail.com>
2024-10-23 11:32:54 +0530

    employee

---
+++ Emp/src/components/EmpList.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+
+const EmpList=()=>{
+    const [details, setDetails]=useState([])
+    const [editing, setEditing]=useState(false)
+    const [currentDetails, setCurrentDetail]=useState({id:null,name:"", address:""})
+
+
+    useEffect
+
+
+
+
+
+}
+
